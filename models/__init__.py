d6c30e26a8bc3803dc9b4ec6bc34b3f980db00d2
AliAhmedML <aliahmedsalah95@gmail.com>
2025-07-19 11:31:57 +0300

    Intiallizing Models

---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+from .diabetes_model import DiabetesModel
+from .heart_model import HeartModel
+from .parkinson_model import ParkinsonModel
+
+diabetes_model = DiabetesModel()
+heart_model = HeartModel()
+parkinson_model = ParkinsonModel()
